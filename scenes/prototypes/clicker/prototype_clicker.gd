c33fc1d93e7a29fcc5edff3fb73c6d7f4c0912bb
Loki31490 <95440838+Loki31490@users.noreply.github.com>
2024-12-14 14:43:26 +0100

    ✨ View navigation

--- scenes/prototypes/clicker/prototype_clicker.gd
+++ scenes/prototypes/clicker/prototype_clicker.gd
@@ -5,7 +5,11 @@ extends Control
 
 ## Reference to the label displaying the current amount of stardust created.
 @export var label : Label
+## View reference.
+@export var view : UserInterface.Views
 
+## Reference to the User Interface.
+@export var user_interface : UserInterface
 
 ## Current amount of stardust created.
 var stardust : int = 0
@@ -14,6 +18,10 @@ var stardust : int = 0
 ## Initialize the label.
 func _ready() -> void:
 	update_label_text()
+	
+	visible = false
+	
+	user_interface.navigation_requested.connect((_on_navigation_request))
 
 
 ## Triggerred when the create stardust button is pressed.
@@ -30,3 +38,11 @@ func update_label_text() -> void :
 func create_stardust() -> void :
 	stardust += 1
 	update_label_text()
+
+## Watch for naviqation request and react accordingly.
+func _on_navigation_request(requested_view : UserInterface.Views) -> void :
+	if requested_view == view:
+		visible = true
+		return
+	
+	visible = false
--- scenes/prototypes/generator/prototype_generator.gd
+++ scenes/prototypes/generator/prototype_generator.gd
@@ -9,6 +9,11 @@ extends Control
 ## Reference to the timer contoling the pace of the generation of stardust.
 @export var timer : Timer
 
+## Reference to the User Interface.
+@export var user_interface : UserInterface
+## View reference.
+@export var view : UserInterface.Views
+
 
 ## Current amount of stardust created.
 var stardust : int = 0
@@ -16,6 +21,10 @@ var stardust : int = 0
 ##Initialize the label.
 func _ready() -> void:
 	update_label_text()
+	
+	visible = true
+	
+	user_interface.navigation_requested.connect((_on_navigation_request))
 
 
 ## Update the text of the label to reflect a change in stardust amount.
@@ -43,3 +52,11 @@ func _on_button_pressed() -> void:
 ## Triggered when the timer times out.
 func _on_timer_timeout() -> void:
 	create_stardust()
+
+## Watch for naviqation request and react accordingly.
+func _on_navigation_request(requested_view : UserInterface.Views) -> void :
+	if requested_view == view:
+		visible = true
+		return
+	
+	visible = false
