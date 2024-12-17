9cf0b6e1e86a22f49871002a7cdc6bb88b1babe6
Loki31490 <95440838+Loki31490@users.noreply.github.com>
2024-12-17 22:59:02 +0100

    EP 11 TC 12:56
    https://www.youtube.com/watch?v=eoeZ_7eR-pI&list=PLgBln8F2Q8BzWYUrRSIkC2RlT-YHf0BQ3&index=16

--- scripts/data/data_cc_upgrades.gd
+++ scripts/data/data_cc_upgrades.gd
@@ -4,3 +4,5 @@ extends Resource
 
 ## CCU 01 Stardust Generation Level.
 @export var u_01_stardust_generation_level : bool = false
+## CCU 01 Stardust Boost Level
+@export var u_02_stardust_boost_level : int = 0
--- scripts/handlers/handler_stardust_generator.gd
+++ scripts/handlers/handler_stardust_generator.gd
@@ -5,6 +5,8 @@ extends Node
 ## Singleton reference.
 static var ref : HandlerStardustGenerator
 
+## Amount of Stardust generated every loop.
+var generator_power : int = 1
 
 ## Singleton check.
 func _enter_tree() -> void:
@@ -25,12 +27,14 @@ func _ready() -> void:
 		timer.start()
 		return
 	
+	calculate_generator_power()
+	
 	HandlerCCUpgrades.ref.upgrade_leveled_up.connect(watch_for_upgrades_level_up)
 
 
 ## Triggered when the timer completes a loop.
 func _on_timer_timeout() -> void:
-	HandlerStardust.ref.create_stardust(1)
+	HandlerStardust.ref.create_stardust(generator_power)
 
 
 ## Wait for ccu_01 to be purchased.
@@ -38,3 +42,11 @@ func watch_for_upgrades_level_up(upgrade : Upgrade) -> void:
 	if upgrade == HandlerCCUpgrades.ref.u_01_stadust_generation:
 		timer.start()
 		HandlerCCUpgrades.ref.upgrade_leveled_up.disconnect(watch_for_upgrades_level_up)
+
+
+## Calculate the amount of stardust calculated every seconds.
+func calculate_generator_power() -> void:
+	var new_power : int = 1
+	new_power += Game.ref.data.cc_upgrades.u_02_stardust_boost_level
+	
+	generator_power = new_power
